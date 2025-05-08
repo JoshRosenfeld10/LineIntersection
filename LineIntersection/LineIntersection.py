0a8be9791e19e1a90ad28796924c98e43a7cc9a6
Josh Rosenfeld <joshr@rogers.com>
2025-05-08 14:27:16 -0400

    init

---
+++ LineIntersection/LineIntersection.py
@@ -0,0 +1,366 @@
+import logging
+import os
+from typing import Annotated, Optional
+
+import vtk
+
+import slicer
+from slicer.i18n import tr as _
+from slicer.i18n import translate
+from slicer.ScriptedLoadableModule import *
+from slicer.util import VTKObservationMixin
+from slicer.parameterNodeWrapper import (
+    parameterNodeWrapper,
+    WithinRange,
+)
+
+import numpy as np
+
+
+#
+# LineIntersection
+#
+
+
+class LineIntersection(ScriptedLoadableModule):
+    """Uses ScriptedLoadableModule base class, available at:
+    https://github.com/Slicer/Slicer/blob/main/Base/Python/slicer/ScriptedLoadableModule.py
+    """
+
+    def __init__(self, parent):
+        ScriptedLoadableModule.__init__(self, parent)
+        # TODO: make this more human readable by adding spaces
+        self.parent.title = _("LineIntersection")
+        # TODO: set categories (folders where the module shows up in the module selector)
+        self.parent.categories = [
+            translate("qSlicerAbstractCoreModule", "Examples")]
+        # TODO: add here list of module names that this module requires
+        self.parent.dependencies = []
+        # TODO: replace with "Firstname Lastname (Organization)"
+        self.parent.contributors = ["John Doe (AnyWare Corp.)"]
+        # TODO: update with short description of the module and a link to online module documentation
+        # _() function marks text as translatable to other languages
+        self.parent.helpText = _("""
+This is an example of scripted loadable module bundled in an extension.
+See more information in <a href="https://github.com/organization/projectname#LineIntersection">module documentation</a>.
+""")
+        # TODO: replace with organization, grant and thanks
+        self.parent.acknowledgementText = _("""
+This file was originally developed by Jean-Christophe Fillion-Robin, Kitware Inc., Andras Lasso, PerkLab,
+and Steve Pieper, Isomics, Inc. and was partially funded by NIH grant 3P41RR013218-12S1.
+""")
+
+        # Additional initialization step after application startup is complete
+        slicer.app.connect("startupCompleted()", registerSampleData)
+
+
+#
+# Register sample data sets in Sample Data module
+#
+
+
+def registerSampleData():
+    """Add data sets to Sample Data module."""
+    # It is always recommended to provide sample data for users to make it easy to try the module,
+    # but if no sample data is available then this method (and associated startupCompeted signal connection) can be removed.
+
+    import SampleData
+
+    iconsPath = os.path.join(os.path.dirname(__file__), "Resources/Icons")
+
+    # To ensure that the source code repository remains small (can be downloaded and installed quickly)
+    # it is recommended to store data sets that are larger than a few MB in a Github release.
+
+    # LineIntersection1
+    SampleData.SampleDataLogic.registerCustomSampleDataSource(
+        # Category and sample name displayed in Sample Data module
+        category="LineIntersection",
+        sampleName="LineIntersection1",
+        # Thumbnail should have size of approximately 260x280 pixels and stored in Resources/Icons folder.
+        # It can be created by Screen Capture module, "Capture all views" option enabled, "Number of images" set to "Single".
+        thumbnailFileName=os.path.join(iconsPath, "LineIntersection1.png"),
+        # Download URL and target file name
+        uris="https://github.com/Slicer/SlicerTestingData/releases/download/SHA256/998cb522173839c78657f4bc0ea907cea09fd04e44601f17c82ea27927937b95",
+        fileNames="LineIntersection1.nrrd",
+        # Checksum to ensure file integrity. Can be computed by this command:
+        #  import hashlib; print(hashlib.sha256(open(filename, "rb").read()).hexdigest())
+        checksums="SHA256:998cb522173839c78657f4bc0ea907cea09fd04e44601f17c82ea27927937b95",
+        # This node name will be used when the data set is loaded
+        nodeNames="LineIntersection1",
+    )
+
+    # LineIntersection2
+    SampleData.SampleDataLogic.registerCustomSampleDataSource(
+        # Category and sample name displayed in Sample Data module
+        category="LineIntersection",
+        sampleName="LineIntersection2",
+        thumbnailFileName=os.path.join(iconsPath, "LineIntersection2.png"),
+        # Download URL and target file name
+        uris="https://github.com/Slicer/SlicerTestingData/releases/download/SHA256/1a64f3f422eb3d1c9b093d1a18da354b13bcf307907c66317e2463ee530b7a97",
+        fileNames="LineIntersection2.nrrd",
+        checksums="SHA256:1a64f3f422eb3d1c9b093d1a18da354b13bcf307907c66317e2463ee530b7a97",
+        # This node name will be used when the data set is loaded
+        nodeNames="LineIntersection2",
+    )
+
+
+#
+# LineIntersectionParameterNode
+#
+
+
+@parameterNodeWrapper
+class LineIntersectionParameterNode:
+    """
+    The parameters needed by module.
+
+    inputVolume - The volume to threshold.
+    imageThreshold - The value at which to threshold the input volume.
+    invertThreshold - If true, will invert the threshold.
+    thresholdedVolume - The output volume that will contain the thresholded volume.
+    invertedVolume - The output volume that will contain the inverted thresholded volume.
+    """
+
+    p1x: int
+    p1y: int
+    p1z: int
+    p2x: int
+    p2y: int
+    p2z: int
+    d1x: int
+    d1y: int
+    d1z: int
+    d2x: int
+    d2y: int
+    d2z: int
+
+
+#
+# LineIntersectionWidget
+#
+
+
+class LineIntersectionWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
+    """Uses ScriptedLoadableModuleWidget base class, available at:
+    https://github.com/Slicer/Slicer/blob/main/Base/Python/slicer/ScriptedLoadableModule.py
+    """
+
+    def __init__(self, parent=None) -> None:
+        """Called when the user opens the module the first time and the widget is initialized."""
+        ScriptedLoadableModuleWidget.__init__(self, parent)
+        # needed for parameter node observation
+        VTKObservationMixin.__init__(self)
+        self.logic = None
+        self._parameterNode = None
+        self._parameterNodeGuiTag = None
+
+    def setup(self) -> None:
+        """Called when the user opens the module the first time and the widget is initialized."""
+        ScriptedLoadableModuleWidget.setup(self)
+
+        # Load widget from .ui file (created by Qt Designer).
+        # Additional widgets can be instantiated manually and added to self.layout.
+        uiWidget = slicer.util.loadUI(
+            self.resourcePath("UI/LineIntersection.ui"))
+        self.layout.addWidget(uiWidget)
+        self.ui = slicer.util.childWidgetVariables(uiWidget)
+
+        # Set scene in MRML widgets. Make sure that in Qt designer the top-level qMRMLWidget's
+        # "mrmlSceneChanged(vtkMRMLScene*)" signal in is connected to each MRML widget's.
+        # "setMRMLScene(vtkMRMLScene*)" slot.
+        uiWidget.setMRMLScene(slicer.mrmlScene)
+
+        # Create logic class. Logic implements all computations that should be possible to run
+        # in batch mode, without a graphical user interface.
+        self.logic = LineIntersectionLogic()
+
+        # Connections
+
+        # These connections ensure that we update parameter node when scene is closed
+        self.addObserver(
+            slicer.mrmlScene, slicer.mrmlScene.StartCloseEvent, self.onSceneStartClose)
+        self.addObserver(slicer.mrmlScene,
+                         slicer.mrmlScene.EndCloseEvent, self.onSceneEndClose)
+
+        # Buttons
+        self.ui.applyButton.connect("clicked(bool)", self.onApplyButton)
+
+        # Make sure parameter node is initialized (needed for module reload)
+        self.initializeParameterNode()
+
+    def cleanup(self) -> None:
+        """Called when the application closes and the module widget is destroyed."""
+        self.removeObservers()
+
+    def enter(self) -> None:
+        """Called each time the user opens this module."""
+        # Make sure parameter node exists and observed
+        self.initializeParameterNode()
+
+    def exit(self) -> None:
+        """Called each time the user opens a different module."""
+        # Do not react to parameter node changes (GUI will be updated when the user enters into the module)
+        if self._parameterNode:
+            self._parameterNode.disconnectGui(self._parameterNodeGuiTag)
+            self._parameterNodeGuiTag = None
+            self.removeObserver(
+                self._parameterNode, vtk.vtkCommand.ModifiedEvent, self._checkCanApply)
+
+    def onSceneStartClose(self, caller, event) -> None:
+        """Called just before the scene is closed."""
+        # Parameter node will be reset, do not use it anymore
+        self.setParameterNode(None)
+
+    def onSceneEndClose(self, caller, event) -> None:
+        """Called just after the scene is closed."""
+        # If this module is shown while the scene is closed then recreate a new parameter node immediately
+        if self.parent.isEntered:
+            self.initializeParameterNode()
+
+    def initializeParameterNode(self) -> None:
+        """Ensure parameter node exists and observed."""
+        # Parameter node stores all user choices in parameter values, node selections, etc.
+        # so that when the scene is saved and reloaded, these settings are restored.
+
+        self.setParameterNode(self.logic.getParameterNode())
+
+    def setParameterNode(self, inputParameterNode: Optional[LineIntersectionParameterNode]) -> None:
+        """
+        Set and observe parameter node.
+        Observation is needed because when the parameter node is changed then the GUI must be updated immediately.
+        """
+
+        if self._parameterNode:
+            self._parameterNode.disconnectGui(self._parameterNodeGuiTag)
+            self.removeObserver(
+                self._parameterNode, vtk.vtkCommand.ModifiedEvent, self._checkCanApply)
+        self._parameterNode = inputParameterNode
+        if self._parameterNode:
+            # Note: in the .ui file, a Qt dynamic property called "SlicerParameterName" is set on each
+            # ui element that needs connection.
+            self._parameterNodeGuiTag = self._parameterNode.connectGui(self.ui)
+            self.addObserver(self._parameterNode,
+                             vtk.vtkCommand.ModifiedEvent, self._checkCanApply)
+            self._checkCanApply()
+
+    def _checkCanApply(self, caller=None, event=None) -> None:
+        if self._parameterNode:
+            self.ui.applyButton.toolTip = _("Compute line intersection")
+            self.ui.applyButton.enabled = True
+        else:
+            self.ui.applyButton.toolTip = _(
+                "Enter values")
+            self.ui.applyButton.enabled = False
+
+    def onApplyButton(self) -> None:
+        """Run processing when user clicks "Apply" button."""
+        with slicer.util.tryWithErrorDisplay(_("Failed to compute results."), waitCursor=True):
+            # Compute output
+            self.logic.process(self.ui.p1x.value, self.ui.p1y.value, self.ui.p1z.value,
+                               self.ui.p2x.value, self.ui.p2y.value, self.ui.p2z.value,
+                               self.ui.d1x.value, self.ui.d1y.value, self.ui.d1z.value,
+                               self.ui.d2x.value, self.ui.d2y.value, self.ui.d2z.value)
+            print(
+                f'Closest point of intersection between lines: {self.logic.intersection}')
+            self.ui.intersectionPointText.text = str(self.logic.intersection)
+
+#
+# LineIntersectionLogic
+#
+
+
+class LineIntersectionLogic(ScriptedLoadableModuleLogic):
+    """This class should implement all the actual
+    computation done by your module.  The interface
+    should be such that other python code can import
+    this class and make use of the functionality without
+    requiring an instance of the Widget.
+    Uses ScriptedLoadableModuleLogic base class, available at:
+    https://github.com/Slicer/Slicer/blob/main/Base/Python/slicer/ScriptedLoadableModule.py
+    """
+
+    def __init__(self) -> None:
+        """Called when the logic class is instantiated. Can be used for initializing member variables."""
+        ScriptedLoadableModuleLogic.__init__(self)
+
+    def getParameterNode(self):
+        return LineIntersectionParameterNode(super().getParameterNode())
+
+    def computeIntersection(self, p1, d1, p2, d2):
+        """
+        Compute the midpoint of the shortest line segment connecting two skew lines in 3D.
+
+        Parameters:
+        - p1, d1: a point and direction vector on line 1
+        - p2, d2: a point and direction vector on line 2
+
+        Returns:
+        - midpoint: the midpoint of the shortest connecting segment between the lines
+        """
+
+        d1 = d1 / np.linalg.norm(d1)
+        d2 = d2 / np.linalg.norm(d2)
+
+        r = p1 - p2
+        a = np.dot(d1, d1)
+        b = np.dot(d1, d2)
+        c = np.dot(d2, d2)
+        d = np.dot(d1, r)
+        e = np.dot(d2, r)
+
+        denom = a * c - b * b
+        if np.isclose(denom, 0):
+            # Lines are parallel; return midpoint between p1 and projection of p2 onto line 1
+            t = 0
+            s = e / c
+        else:
+            t = (b * e - c * d) / denom
+            s = (a * e - b * d) / denom
+
+        closest_point1 = p1 + t * d1
+        closest_point2 = p2 + s * d2
+
+        midpoint = (closest_point1 + closest_point2) / 2
+        return midpoint
+
+    def process(self, p1x, p1y, p1z, p2x, p2y, p2z, d1x, d1y, d1z, d2x, d2y, d2z):
+        self.intersection = self.computeIntersection(np.array([p1x, p1y, p1z]), np.array([d1x, d1y, d1z]), np.array([
+            p2x, p2y, p2z]), np.array([d2x, d2y, d2z]))
+
+        return True
+
+
+#
+# LineIntersectionTest
+#
+
+
+class LineIntersectionTest(ScriptedLoadableModuleTest):
+    """
+    This is the test case for your scripted module.
+    Uses ScriptedLoadableModuleTest base class, available at:
+    https://github.com/Slicer/Slicer/blob/main/Base/Python/slicer/ScriptedLoadableModule.py
+    """
+
+    def setUp(self):
+        """Do whatever is needed to reset the state - typically a scene clear will be enough."""
+        slicer.mrmlScene.Clear()
+
+    def runTest(self):
+        """Run as few or as many tests as needed here."""
+        self.setUp()
+        self.test_LineIntersection1()
+
+    def test_LineIntersection1(self):
+        """Ideally you should have several levels of tests.  At the lowest level
+        tests should exercise the functionality of the logic with different inputs
+        (both valid and invalid).  At higher levels your tests should emulate the
+        way the user would interact with your code and confirm that it still works
+        the way you intended.
+        One of the most important features of the tests is that it should alert other
+        developers when their changes will have an impact on the behavior of your
+        module.  For example, if a developer removes a feature that you depend on,
+        your test should break so they know that the feature is needed.
+        """
+
+        return
